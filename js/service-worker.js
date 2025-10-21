8a2213c9295c74e3827b8f13f2eb754005e833e5
Volodymyr Mykhalchuk <vmykhalchuk@vertafore.com>
2025-10-21 17:59:57 +0300

    .

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,35 +1,24 @@
-const filesToCache = [
-  "/",
-  "js/main.js"//,
-  //"css/master.css",
-  //"https://fonts.googleapis.com/css2?family=Spartan:wght@700&display=swap"
-];
-
-//const staticCacheName = "http://127.0.0.1:8887";
-
 // install event
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== staticCacheName).map((key) => caches.delete(key))
-      );
-    })
-  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(clientList => {
+      for (const client of clientList) {
+        if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
     })
   );
 });
